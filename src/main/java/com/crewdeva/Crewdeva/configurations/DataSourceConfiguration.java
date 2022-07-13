1848acdf170dfba08d254b8beee43f28175086f3
Anton Shvechikhin <antonkamski@gmail.com>
2022-07-13 16:09:05 +0400

    Hibernate connected

--- src/main/java/com/crewdeva/Crewdeva/configurations/DataSourceConfiguration.java
+++ src/main/java/com/crewdeva/Crewdeva/configurations/DataSourceConfiguration.java
@@ -3,8 +3,12 @@ package com.crewdeva.Crewdeva.configurations;
 import org.springframework.boot.jdbc.DataSourceBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.orm.jpa.JpaVendorAdapter;
+import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
+import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
 
 import javax.sql.DataSource;
+import java.util.Properties;
 
 @Configuration
 public class DataSourceConfiguration {
@@ -16,6 +20,12 @@ public class DataSourceConfiguration {
         private static final String POSTGRES_PASSWORD = "postgres";
     }
 
+    private static abstract class HibernateProperties {
+        private static final String AUTO_DDL_EXECUTION = "hibernate.hbm2ddl.auto";
+        private static final String SHOW_DDL_LOG = "hibernate.show_sql";
+        private static final String DIALECT = "dialect";
+    }
+
     @Bean
     public DataSource getDataSource() {
         DataSourceBuilder<?> dataSourceBuilder = DataSourceBuilder.create();
@@ -24,5 +34,23 @@ public class DataSourceConfiguration {
         dataSourceBuilder.username(DatabaseParams.POSTGRES_USERNAME);
         dataSourceBuilder.password(DatabaseParams.POSTGRES_PASSWORD);
         return dataSourceBuilder.build();
+    }
+
+    @Bean
+    public LocalContainerEntityManagerFactoryBean entityManagerFactory()
+    {
+        LocalContainerEntityManagerFactoryBean em = new LocalContainerEntityManagerFactoryBean();
+        em.setDataSource(getDataSource());
+        em.setPackagesToScan("com.crewdeva.Crewdeva.models");
+
+        JpaVendorAdapter vendorAdapter = new HibernateJpaVendorAdapter();
+        em.setJpaVendorAdapter(vendorAdapter);
+        Properties jpaProperties = new Properties();
+        jpaProperties.put(HibernateProperties.AUTO_DDL_EXECUTION, "update");
+        jpaProperties.put(HibernateProperties.SHOW_DDL_LOG, "true");
+        jpaProperties.put(HibernateProperties.DIALECT, "org.hibernate.dialect.PostgreSQL10Dialect");
+        em.setJpaProperties(jpaProperties);
+
+        return em;
     }
 }
